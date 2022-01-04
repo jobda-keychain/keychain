f7b12dd28f1e0c5e7131695a87ccdbdf64e26497
silver-deer <hsasy0113@dgsw.hs.kr>
2022-01-04 16:58:18 +0900

    init

---
+++ src/main/java/com/jobda/keychain/KeychainApplication.java
@@ -0,0 +1,13 @@
+package com.jobda.keychain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeychainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeychainApplication.class, args);
+    }
+
+}
