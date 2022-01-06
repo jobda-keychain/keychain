711990ff692ef60dfaef143d40027f12e556a9eb
syxxn <yyuunn17@naver.com>
2022-01-06 16:48:21 +0900

    [ADD] (#20) AccountEnvironment DAO 추가

---
+++ src/main/java/com/jobda/keychain/entity/account_environment/AccountEnvironment.java
@@ -0,0 +1,35 @@
+package com.jobda.keychain.entity.account_environment;
+
+import com.jobda.keychain.entity.account.Account;
+import com.jobda.keychain.entity.environment.Environment;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class AccountEnvironment {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id", nullable = false)
+    private Account account;
+
+    @ManyToOne
+    @JoinColumn(name = "environment_id", nullable = false)
+    private Environment environment;
+
+    private AccountEnvironment(Account account, Environment environment) {
+        this.account = account;
+        this.environment = environment;
+    }
+
+    public static AccountEnvironment createAccountEnvironment(/*request*/) {
+        return new AccountEnvironment(null, null);
+    }
+
+}
