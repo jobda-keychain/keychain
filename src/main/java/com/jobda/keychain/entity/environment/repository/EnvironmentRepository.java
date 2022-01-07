6243017b93f964b01b976d6bf7852e04589ba874
syxxn <yyuunn17@naver.com>
2022-01-07 09:15:06 +0900

    [UPDATE] (#28) Environment 엔티티 pk 타입 String -> Long

--- src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepository.java
+++ src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface EnvironmentRepository extends CrudRepository<Environment, String> {
+public interface EnvironmentRepository extends CrudRepository<Environment, Long> {
 }
