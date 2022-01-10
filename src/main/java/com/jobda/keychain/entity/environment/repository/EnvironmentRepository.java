38b4160fd884e3eb797f6452fe1a3aff0e5284e3
syxxn <yyuunn17@naver.com>
2022-01-10 16:18:22 +0900

    [ADD] (#44) 테스트에서 사용할 메소드 추가

--- src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepository.java
+++ src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepository.java
@@ -4,6 +4,9 @@ import com.jobda.keychain.entity.environment.Environment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface EnvironmentRepository extends CrudRepository<Environment, Long> {
+    List<Environment> findAllBy();
 }
