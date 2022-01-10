4b6224371cf6de5959b5d73113b676800bdba0bf
syxxn <yyuunn17@naver.com>
2022-01-10 15:11:10 +0900

    [ADD] (#44) AddEnvironmentRequest 클래스 추가

---
+++ src/main/java/com/jobda/keychain/dto/request/AddEnvironmentRequest.java
@@ -0,0 +1,27 @@
+package com.jobda.keychain.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class AddEnvironmentRequest {
+
+    @Length(min = 2, max = 10)
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String serverDomain;
+
+    @NotBlank
+    private String clientDomain;
+
+    @NotBlank
+    private String platform;
+
+}
