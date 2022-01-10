53cbe9d4d0e5e76d45b955d771cb9c6184b186f1
syxxn <yyuunn17@naver.com>
2022-01-10 11:28:27 +0900

    [UPDATE] (#46) EXCEPTION 상수화

--- src/main/java/com/jobda/keychain/exception/UserNotFoundException.java
+++ src/main/java/com/jobda/keychain/exception/UserNotFoundException.java
@@ -3,7 +3,7 @@ package com.jobda.keychain.exception;
 import com.jobda.keychain.exception.handler.KeyChainException;
 
 public class UserNotFoundException extends KeyChainException {
-    private final UserNotFoundException EXCEPTION = new UserNotFoundException();
+    private static final UserNotFoundException EXCEPTION = new UserNotFoundException();
 
     protected UserNotFoundException() {
         super(404, "User not found");
