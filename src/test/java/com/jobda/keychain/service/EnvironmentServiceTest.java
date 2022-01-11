5c38b08f799d172e14de0dd49a94576d05be7aa3
이승윤 <yyuunn17@naver.com>
2022-01-11 20:42:47 +0900

    [DELETE] (#44) 사용하지 않는 의존성 삭제

--- src/test/java/com/jobda/keychain/service/EnvironmentServiceTest.java
+++ src/test/java/com/jobda/keychain/service/EnvironmentServiceTest.java
@@ -5,13 +5,10 @@ import com.jobda.keychain.entity.environment.Environment;
 import com.jobda.keychain.entity.platform.Platform;
 import com.jobda.keychain.entity.platform.ServiceType;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ExtendWith(MockitoExtension.class)
 class EnvironmentServiceTest {
 
     @Test
