de70812831b9231380dea1315064bfb16b47732e
syxxn <yyuunn17@naver.com>
2022-01-12 14:36:12 +0900

    [ADD] (#38) JPA 메소드 추가 - 환경으로 계정 리스트 가져오기

--- src/main/java/com/jobda/keychain/entity/account/repository/AccountRepository.java
+++ src/main/java/com/jobda/keychain/entity/account/repository/AccountRepository.java
@@ -1,10 +1,13 @@
 package com.jobda.keychain.entity.account.repository;
 
 import com.jobda.keychain.entity.account.Account;
+import com.jobda.keychain.entity.environment.Environment;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
-
+    List<Account> findAllByEnvironment(Environment environment);
 }
