3932cbdffc501dd5b018521503581c6ad19b9461
syxxn <yyuunn17@naver.com>
2022-01-13 10:43:59 +0900

    [ADD] (#51) @Transactional(readOnly=true)

--- src/main/java/com/jobda/keychain/service/EnvironmentService.java
+++ src/main/java/com/jobda/keychain/service/EnvironmentService.java
@@ -12,6 +12,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.stream.Collectors;
 
@@ -36,6 +37,7 @@ public class EnvironmentService {
         environmentRepository.save(environment);
     }
 
+    @Transactional(readOnly = true)
     public EnvironmentsResponse getEnvironments(Pageable page) {
         Page<Environment> environmentPage = environmentRepository.findAllBy(page);
 
