f3796ddf9a867448fcb1129471482f6527b45607
syxxn <yyuunn17@naver.com>
2022-01-14 11:08:58 +0900

    [UPDATE] (#71) import 오류 수정

--- src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepositoryExtension.java
+++ src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepositoryExtension.java
@@ -2,6 +2,8 @@ package com.jobda.keychain.entity.environment.repository;
 
 import com.jobda.keychain.entity.environment.Environment;
 import com.jobda.keychain.entity.platform.ServiceType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
