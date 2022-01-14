c19bf3e85de48ad4c7bb79f57d2c7ed2416c3ea3
사승은 <hsasy0113@dgsw.hs.kr>
2022-01-14 15:26:31 +0900

    [FIX] 기본 생성자 추가

--- src/main/java/com/jobda/keychain/dto/request/CreateAccountRequest.java
+++ src/main/java/com/jobda/keychain/dto/request/CreateAccountRequest.java
@@ -1,13 +1,16 @@
 package com.jobda.keychain.dto.request;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class CreateAccountRequest {
 
--- src/main/java/com/jobda/keychain/dto/request/UpdateAccountRequest.java
+++ src/main/java/com/jobda/keychain/dto/request/UpdateAccountRequest.java
@@ -7,6 +7,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class UpdateAccountRequest {
 
