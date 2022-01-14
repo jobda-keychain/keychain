2536176f99e2ba52cf7e5423195f19cc1acd6d2d
syxxn <yyuunn17@naver.com>
2022-01-14 12:04:14 +0900

    [UPDATE] (#71) selectFrom 사용

--- src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepositoryImpl.java
+++ src/main/java/com/jobda/keychain/entity/environment/repository/EnvironmentRepositoryImpl.java
@@ -3,10 +3,6 @@ package com.jobda.keychain.entity.environment.repository;
 import com.jobda.keychain.entity.environment.Environment;
 import com.jobda.keychain.entity.platform.ServiceType;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
-
-import java.util.List;
 import com.querydsl.jpa.JPQLQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.data.domain.Page;
@@ -14,7 +10,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.support.Querydsl;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Objects;
@@ -33,21 +28,19 @@ public class EnvironmentRepositoryImpl extends QuerydslRepositorySupport impleme
 
     @Override
     public List<Environment> findAllByPlatformEnvironments(ServiceType platformType) {
-        return queryFactory.select(environment).from(environment)
+        return queryFactory.selectFrom(environment)
                 .join(environment.platform, platform)
                 .on(platform.eq(environment.platform))
                 .where(platformTypeEq(platformType))
                 .fetch();
     }
-  
+
     @Override
     public Page<Environment> findAllByPlatformEnvironment(Pageable page) {
         JPQLQuery<Environment> query = querydsl().applyPagination(page,
-                queryFactory.select(
-                        environment
-                )).from(environment)
-                .join(environment.platform, platform)
-                .on(platform.name.eq(environment.platform.name));
+                queryFactory.selectFrom(environment)
+                        .join(environment.platform, platform)
+                        .on(platform.name.eq(environment.platform.name)));
         List<Environment> list = query.fetch();
 
         return new PageImpl<>(list, page, query.fetchCount());
@@ -56,12 +49,12 @@ public class EnvironmentRepositoryImpl extends QuerydslRepositorySupport impleme
     private Querydsl querydsl() {
         return Objects.requireNonNull(getQuerydsl());
     }
-  
+
     private BooleanExpression platformTypeEq(ServiceType platformType) {
         if (platformType == null) {
             return null;
         }
         return platform.name.eq(platformType);
     }
-  
+
 }
