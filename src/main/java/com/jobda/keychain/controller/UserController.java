9f7ba07906008a0c1d6ff548abce3908e4ce12ab
syxxn <yyuunn17@naver.com>
2022-01-14 11:36:48 +0900

    [UPDATE] 머지 충돌 오류 수정

--- src/main/java/com/jobda/keychain/controller/UserController.java
+++ src/main/java/com/jobda/keychain/controller/UserController.java
@@ -1,18 +1,23 @@
 package com.jobda.keychain.controller;
 
+import com.jobda.keychain.dto.request.CreateAccountRequest;
+import com.jobda.keychain.dto.request.UpdateAccountRequest;
 import com.jobda.keychain.dto.response.DetailsResponse;
 import com.jobda.keychain.dto.response.TokenResponse;
 import com.jobda.keychain.dto.response.UpdateAccountResponse;
 import com.jobda.keychain.entity.account.Account;
-import com.jobda.keychain.dto.request.CreateUserRequest;
-import com.jobda.keychain.dto.request.UpdateUserRequest;
-import com.jobda.keychain.exception.DataNotFoundException;
-import com.jobda.keychain.dto.request.CreateAccountRequest;
-import com.jobda.keychain.dto.request.UpdateAccountRequest;
 import com.jobda.keychain.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -53,9 +58,9 @@ public class UserController {
     public void deleteUser(@PathVariable long id) {
         userService.deleteUser(id);
     }
-  
+
     @GetMapping("/details/{id}")
-    public DetailsResponse detailsUser(@PathVariable long id){
+    public DetailsResponse detailsUser(@PathVariable long id) {
         return userService.detailsUser(id);
     }
 
--- src/main/java/com/jobda/keychain/service/UserService.java
+++ src/main/java/com/jobda/keychain/service/UserService.java
@@ -1,38 +1,27 @@
 package com.jobda.keychain.service;
 
 import com.jobda.keychain.AuthApiClient;
-import com.jobda.keychain.dto.request.CreateUserRequest;
+import com.jobda.keychain.dto.request.CreateAccountRequest;
 import com.jobda.keychain.dto.request.LoginApiRequest;
-import com.jobda.keychain.dto.request.UpdateUserRequest;
-import com.jobda.keychain.entity.account.Account;
-import com.jobda.keychain.entity.account.repository.AccountRepository;
-import com.jobda.keychain.exception.DataNotFoundException;
-import com.jobda.keychain.exception.UnableLoginException;
+import com.jobda.keychain.dto.request.UpdateAccountRequest;
 import com.jobda.keychain.dto.response.DetailsResponse;
-import com.jobda.keychain.dto.response.PlatformEnvironmentsResponse;
 import com.jobda.keychain.dto.response.TokenResponse;
 import com.jobda.keychain.dto.response.UpdateAccountResponse;
 import com.jobda.keychain.entity.account.Account;
 import com.jobda.keychain.entity.account.repository.AccountRepository;
 import com.jobda.keychain.entity.environment.Environment;
+import com.jobda.keychain.entity.environment.repository.EnvironmentRepository;
 import com.jobda.keychain.entity.platform.ServiceType;
-import com.jobda.keychain.exception.AlreadyDataExistsException;
 import com.jobda.keychain.exception.DataNotFoundException;
 import com.jobda.keychain.exception.UnableLoginException;
-import com.jobda.keychain.dto.request.UpdateAccountRequest;
-import com.jobda.keychain.dto.request.CreateAccountRequest;
-import com.jobda.keychain.entity.environment.repository.EnvironmentRepository;
-
 import feign.FeignException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -46,11 +35,11 @@ public class UserService {
 
 
     /**
-    * 외부 로그인 API 호출 메서드
-    * 성공 시 Token 발급, 실패 시 UnableLoginException 발생
-    *
-    * @author: sse
-    **/
+     * 외부 로그인 API 호출 메서드
+     * 성공 시 Token 발급, 실패 시 UnableLoginException 발생
+     *
+     * @author: sse
+     **/
     public String callLoginApi(String id, String password, String serverDomain) {
         URI uri = URI.create(serverDomain);
         LoginApiRequest apiRequest = new LoginApiRequest(id, password);
@@ -73,7 +62,7 @@ public class UserService {
 
         String token = callLoginApi(account.getUserId(), account.getPassword(), environment.getServerDomain());
 
-        if(token == null || token.isBlank()) {
+        if (token == null || token.isBlank()) {
             throw UnableLoginException.EXCEPTION;
         }
 
@@ -81,17 +70,17 @@ public class UserService {
     }
 
     /**
-    * 계정 정보 수정
-    * 성공하면 수정된 정보 반환
-    * 계정 정보가 없으면 404 Not Found 발생
-    * 로그인 실패 시 UnableLoginException 발생
-    * Account Entity 중복 발생 시 409 Conflict 발생
-    *
-    * @author: sse
-    **/
+     * 계정 정보 수정
+     * 성공하면 수정된 정보 반환
+     * 계정 정보가 없으면 404 Not Found 발생
+     * 로그인 실패 시 UnableLoginException 발생
+     * Account Entity 중복 발생 시 409 Conflict 발생
+     *
+     * @author: sse
+     **/
     @Transactional
     public UpdateAccountResponse updateUser(long id, UpdateAccountRequest request) {
-        Account account = accountRepository.findById(id).orElseThrow(()-> new DataNotFoundException("User not found"));
+        Account account = accountRepository.findById(id).orElseThrow(() -> new DataNotFoundException("User not found"));
 
         account.changeInfo(request.getUserId(), request.getPassword(), request.getDescription());
 
@@ -99,7 +88,7 @@ public class UserService {
 
         String token = callLoginApi(account.getUserId(), account.getPassword(), environment.getServerDomain());
 
-        if(token == null || token.isBlank()) {
+        if (token == null || token.isBlank()) {
             throw UnableLoginException.EXCEPTION;
         }
 
@@ -121,13 +110,13 @@ public class UserService {
         return new DetailsResponse(account.getId(), account.getUserId(), account.getPassword(), platform, environment, account.getDescription());
     }
 
-    public List<Account> selectUser(){
+    public List<Account> selectUser() {
         //response dto를 만들어서 반환하는 것이 좋을 것 같음.
         //return accountRepository.findAll();
         return new ArrayList<>();
     }
 
-    public void deleteUser(long id){
+    public void deleteUser(long id) {
         accountRepository.findById(id).orElseThrow(() -> {
             throw new DataNotFoundException("userId Not Found");
         });
@@ -135,14 +124,14 @@ public class UserService {
     }
 
     /**
-    * account id를 매개변수로 받고
-    * 성공 시 TokenResponse 반환
-    *
-    * @author: sse
-    **/
+     * account id를 매개변수로 받고
+     * 성공 시 TokenResponse 반환
+     *
+     * @author: sse
+     **/
     public TokenResponse getToken(Long id) {
 
-        Account account = accountRepository.findById(id).orElseThrow(()-> new DataNotFoundException("User is not found"));
+        Account account = accountRepository.findById(id).orElseThrow(() -> new DataNotFoundException("User is not found"));
 
         String token = callLoginApi(account.getUserId(), account.getPassword(), account.getEnvironment().getServerDomain());
 
