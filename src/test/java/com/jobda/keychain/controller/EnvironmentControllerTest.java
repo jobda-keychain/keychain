8c77b0bb9fabfb6995121714904837adb29aff8a
syxxn <yyuunn17@naver.com>
2022-01-17 13:43:34 +0900

    [DELETE] (#52) entity manager 의존성 삭제

--- src/test/java/com/jobda/keychain/controller/EnvironmentControllerTest.java
+++ src/test/java/com/jobda/keychain/controller/EnvironmentControllerTest.java
@@ -20,7 +20,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
