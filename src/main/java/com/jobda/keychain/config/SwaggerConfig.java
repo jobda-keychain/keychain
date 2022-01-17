17b72be750d0ecd30362a5860ee22dae39e770d0
사승은 <hsasy0113@dgsw.hs.kr>
2022-01-17 15:33:00 +0900

    [UPDATE] profiles activie naming

--- src/main/java/com/jobda/keychain/config/SwaggerConfig.java
+++ src/main/java/com/jobda/keychain/config/SwaggerConfig.java
@@ -9,7 +9,7 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
-@Profile("!production")
+@Profile("!prod")
 @Configuration
 public class SwaggerConfig {
 
