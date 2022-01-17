c171f2d9e268c4175326cb927f6e15239f5ef5f8
사승은 <hsasy0113@dgsw.hs.kr>
2022-01-17 15:06:44 +0900

    [UPDATE] Swagger Property Setting

--- src/main/java/com/jobda/keychain/config/SwaggerConfig.java
+++ src/main/java/com/jobda/keychain/config/SwaggerConfig.java
@@ -2,22 +2,21 @@ package com.jobda.keychain.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+@Profile("!production")
 @Configuration
-@EnableWebMvc
 public class SwaggerConfig {
 
     private static final String API_NAME = "KEYCHAIN API";
     private static final String API_VERSION = "0.0.1";
     private static final String API_DESCRIPTION = "JOBDA Keychain Rest API Documentation";
     private static final String API_BASE_URL = "localhost:8080";
-
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
@@ -29,7 +28,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2)
+        return new Docket(DocumentationType.OAS_30)
                 .groupName("Keychain API")
                 .apiInfo(apiInfo())
                 .host(API_BASE_URL)
