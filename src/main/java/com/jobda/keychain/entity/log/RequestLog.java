1cf9675067a214a293f4d6b5693cf755b5d8c5e7
syxxn <yyuunn17@naver.com>
2022-01-19 14:42:45 +0900

    [UPDATE] (#92) createdAt 분리

--- src/main/java/com/jobda/keychain/entity/log/RequestLog.java
+++ src/main/java/com/jobda/keychain/entity/log/RequestLog.java
@@ -1,32 +1,24 @@
 package com.jobda.keychain.entity.log;
 
+import com.jobda.keychain.entity.BaseCreatedAtEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.EntityListeners;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor
-@EntityListeners(AuditingEntityListener.class)
 @Entity
-public class RequestLog {
+public class RequestLog extends BaseCreatedAtEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-    @CreatedDate
-    @Column(nullable = false)
-    private LocalDateTime requestedAt;
 
     @Enumerated(EnumType.STRING)
     @Column(length = 18, nullable = false)
