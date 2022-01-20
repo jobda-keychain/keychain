eb8c05dbdc06f717b60d5bfc115500228e7b3fcd
Lee MinJi <m04j00@gmail.com>
2022-01-20 15:28:16 +0900

    [UPDATE] (#168) 상세 정보 response에 환경 정보 수정

--- src/main/java/com/jobda/keychain/dto/response/DetailsResponse.java
+++ src/main/java/com/jobda/keychain/dto/response/DetailsResponse.java
@@ -2,7 +2,6 @@ package com.jobda.keychain.dto.response;
 
 import com.jobda.keychain.entity.account.Account;
 import com.jobda.keychain.entity.platform.PlatformType;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
@@ -16,19 +15,18 @@ public class DetailsResponse {
 
     private PlatformType platform;
 
-    private String environment;
+    private DetailsAccountEnvironmentDto environment;
 
     private String description;
 
-    public static DetailsResponse of(Account account){
+    public static DetailsResponse of(Account account) {
         DetailsResponse detailsResponse = new DetailsResponse();
         detailsResponse.id = account.getId();
         detailsResponse.userId = account.getUserId();
         detailsResponse.password = account.getPassword();
         detailsResponse.platform = account.getEnvironment().getPlatform().getName();
-        detailsResponse.environment = account.getEnvironment().getName();
+        detailsResponse.environment = DetailsAccountEnvironmentDto.of(account.getEnvironment());
         detailsResponse.description = account.getDescription();
         return detailsResponse;
     }
-
 }
