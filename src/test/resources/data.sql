21973eeea511a4f3cb750ecf10c2a54b734b5a30
Lee MinJi <m04j00@gmail.com>
2022-01-20 09:58:58 +0900

    [DELETE] (#98) 더미데이터 삭제

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -10,7 +10,3 @@ INSERT INTO account(user_id, password, description, environment_id) values('test
 INSERT INTO account(user_id, password, description, environment_id) values('test2', 'test2', 'JOBDA, dv', 1);
 INSERT INTO account(user_id, password, description, environment_id) values('test3', 'test3', 'JOBDA, dv-1', 2);
 INSERT INTO account(user_id, password, description, environment_id) values('test4', 'test4', 'JOBDA, st', 3);
-INSERT INTO account(user_id, password, description, environment_id) values('test5', 'test5', 'JOBDA_CMS, dv', 4);
-INSERT INTO account(user_id, password, description, environment_id) values('test6', 'test6', 'JOBDA_CMS, dv', 4);
-INSERT INTO account(user_id, password, description, environment_id) values('test7', 'test7', 'JOBDA_CMS, dv', 4);
-INSERT INTO account(user_id, password, description, environment_id) values('test8', 'test8', 'JOBDA_CMS, dv', 4);
