ec95d2be0b65c777bb408734be01af480aeacfd6
이승윤 <yyuunn17@naver.com>
2022-01-21 21:06:24 +0900

    [UPDATE] (#139) service에서 처리하던 것 Event로 분리

---
+++ src/main/java/com/jobda/keychain/event/handler/LogEventHandler.java
@@ -0,0 +1,23 @@
+package com.jobda.keychain.event.handler;
+
+import com.jobda.keychain.entity.log.MethodType;
+import com.jobda.keychain.entity.log.RequestLog;
+import com.jobda.keychain.entity.log.repository.RequestLogRepository;
+import com.jobda.keychain.event.LogEvent;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class LogEventHandler {
+
+    private final RequestLogRepository requestLogRepository;
+
+    @EventListener
+    public void saveRequestLog(LogEvent logEvent) {
+        RequestLog requestLog = RequestLog.createLog(logEvent.getMethodType(), logEvent.getClientIpAddress());
+        requestLogRepository.save(requestLog);
+    }
+
+}
--- src/main/java/com/jobda/keychain/service/LogService.java
+++ src/main/java/com/jobda/keychain/service/LogService.java
@@ -1,26 +1,21 @@
 package com.jobda.keychain.service;
 
-import com.jobda.keychain.entity.log.RequestLog;
 import com.jobda.keychain.entity.log.MethodType;
-import com.jobda.keychain.entity.log.repository.RequestLogRepository;
+import com.jobda.keychain.event.LogEvent;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @Service
 public class LogService {
 
-    private final RequestLogRepository requestLogRepository;
+    private final ApplicationEventPublisher publisher;
 
     @Transactional
     public void saveRequestLog(String clientIpAddress, MethodType methodType) {
-        String clientAddress = clientIpAddress;
-
-        RequestLog requestLog = RequestLog.createLog(methodType, clientAddress);
-        requestLogRepository.save(requestLog);
+        publisher.publishEvent(new LogEvent(clientIpAddress, methodType));
     }
 
 }
