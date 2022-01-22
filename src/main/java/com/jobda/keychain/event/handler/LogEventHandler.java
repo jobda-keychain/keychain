6fdca3d3ef6f569f316109d7e9a7aa7d4b149e67
이승윤 <yyuunn17@naver.com>
2022-01-22 13:01:20 +0900

    [UPDATE] (#139) @TransactionalEventListener로 변경 및 저장 로직 삭제

--- src/main/java/com/jobda/keychain/event/handler/LogEventHandler.java
+++ src/main/java/com/jobda/keychain/event/handler/LogEventHandler.java
@@ -1,25 +1,22 @@
 package com.jobda.keychain.event.handler;
 
-import com.jobda.keychain.entity.log.MethodType;
 import com.jobda.keychain.entity.log.RequestLog;
 import com.jobda.keychain.entity.log.repository.RequestLogRepository;
 import com.jobda.keychain.event.LogEvent;
+import com.jobda.keychain.service.LogService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 @RequiredArgsConstructor
 @Component
 public class LogEventHandler {
 
-    private final RequestLogRepository requestLogRepository;
+    private final LogService logService;
 
-    @Async
-    @EventListener
+    @TransactionalEventListener
     public void saveRequestLog(LogEvent logEvent) {
-        RequestLog requestLog = RequestLog.createLog(logEvent.getMethodType(), logEvent.getClientIpAddress());
-        requestLogRepository.save(requestLog);
+        logService.saveRequestLog(logEvent.getMethodType(), logEvent.getClientIpAddress());
     }
 
 }
