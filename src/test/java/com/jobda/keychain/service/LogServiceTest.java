e2dda415bba4b11b30de0042f53c7d3f8ec41b3c
syxxn <yyuunn17@naver.com>
2022-01-25 14:15:44 +0900

    [ADD] (#180) LogServiceTest 추가

---
+++ src/test/java/com/jobda/keychain/service/LogServiceTest.java
@@ -0,0 +1,23 @@
+package com.jobda.keychain.service;
+
+import com.jobda.keychain.KeychainApplication;
+import com.jobda.keychain.entity.log.MethodType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@SpringBootTest(classes = KeychainApplication.class)
+class LogServiceTest {
+
+    @Autowired
+    private LogService logService;
+
+    @Test
+    void saveLogTest() {
+        logService.saveRequestLog(MethodType.ADD_ENVIRONMENT, "http://localhost:8080");
+    }
+
+}
