1959e0fa60549432e4802086c73d92fdce01452b
syxxn <yyuunn17@naver.com>
2022-01-25 14:26:31 +0900

    [DELETE] (#180) @Transactional

--- src/test/java/com/jobda/keychain/service/LogServiceTest.java
+++ src/test/java/com/jobda/keychain/service/LogServiceTest.java
@@ -6,9 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.transaction.Transactional;
-
-@Transactional
 @SpringBootTest(classes = KeychainApplication.class)
 class LogServiceTest {
 
