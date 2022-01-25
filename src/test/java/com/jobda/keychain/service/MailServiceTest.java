944fb0eed333fbb6f4cad1bc8c4ab9138129029e
syxxn <yyuunn17@naver.com>
2022-01-25 15:14:07 +0900

    [DELETE] (#180) @Transactonal

--- src/test/java/com/jobda/keychain/service/MailServiceTest.java
+++ src/test/java/com/jobda/keychain/service/MailServiceTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest(classes = KeychainApplication.class)
 @ActiveProfiles("test")
 class MailServiceTest {
