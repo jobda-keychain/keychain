8e96dddb10bae81f3ccdb7256dd53e43b46e4ed5
syxxn <yyuunn17@naver.com>
2022-01-25 14:44:23 +0900

    [ADD] (#180) mail send method test

---
+++ src/test/java/com/jobda/keychain/service/MailServiceTest.java
@@ -0,0 +1,21 @@
+package com.jobda.keychain.service;
+
+import com.jobda.keychain.KeychainApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(classes = KeychainApplication.class)
+@ActiveProfiles("test")
+class MailServiceTest {
+
+    @Autowired
+    private MailService mailService;
+
+    @Test
+    void sendMailTest() {
+        mailService.sendMail("test@gmail.com");
+    }
+
+}
