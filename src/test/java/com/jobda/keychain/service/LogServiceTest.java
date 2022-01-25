cd99c565b6567c2d70e2db6ce821202ab295b2e1
syxxn <yyuunn17@naver.com>
2022-01-25 14:37:03 +0900

    [UPDATE] (#180) fix test error

--- src/test/java/com/jobda/keychain/service/LogServiceTest.java
+++ src/test/java/com/jobda/keychain/service/LogServiceTest.java
@@ -5,10 +5,12 @@ import com.jobda.keychain.entity.log.MethodType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
 @SpringBootTest(classes = KeychainApplication.class)
+@ActiveProfiles("test")
 class LogServiceTest {
 
     @Autowired
