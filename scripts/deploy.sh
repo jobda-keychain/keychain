ff8492e2ff780bcc3694d6eea5b0283c99c04af9
Min J <69101860+m04j00@users.noreply.github.com>
2022-01-26 14:30:13 +0900

    [UPDATE] 8080 port pid 제거

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,13 +2,15 @@
 
 REPOSITORY=/home/ubuntu/app
 
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
 echo "> 현재 구동 중인 app pid : "
 
-CURRENT_PID=$ ps ax | grep java | awk '{print $1}'
+CURRENT_PID=$ pgrep -fl ${JAR_NAME##*/} | grep java | awk '{print $1}'
 
 echo "현재 구동 중인 app pid : $CURRENT_PID"
 
-if [ "$CURRENT_PID" ]; then
+if [ ${CURRENT_PID} ]; then
   echo "현재 구동 중인 app이 없으므로 종료하지 않습니다."
 else
   echo "> kill -9 $CURRENT_PID"
@@ -18,8 +20,6 @@ fi
 
 echo "> 새 app 배포"
 
-JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
-
 echo "> JAR NAME: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
@@ -28,4 +28,4 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
-nohup java -jar $REPOSITORY/$JAR_NAME &
+nohup java -jar $JAR_NAME &
